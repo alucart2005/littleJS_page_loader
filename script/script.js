0ffdf92930d9d0447dc9d7d7993cd8fb2cf589a7
Napoleon Anaya R <napoleonanaya@gmail.com>
2023-07-18 15:25:52 -0500

    feat: add new feature and preview

---
+++ script/script.js
@@ -0,0 +1,5 @@
+setTimeout(function() {
+    //document.querySelector('.page-loader').style.display = 'none';
+    //document.querySelector('.page-loader').classList.add('hola');
+    document.querySelector('.page-loader').remove();
+},3000);
